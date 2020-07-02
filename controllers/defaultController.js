b4a9238c99573c755c1266da3069da3d7269f548
navruz-zbx <iminov.zbx@gmail.com>
2020-07-03 01:15:38 +0500

    Register validation is ready

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,5 +1,7 @@
 const Post = require('../models/PostModel').Post;
 const Category = require('../models/CategoryModel').Category;
+const User = require('../models/UserModel').User;
+const bcrypt = require('bcryptjs');
 
 
 module.exports = {
@@ -20,6 +22,51 @@ module.exports = {
         res.render('default/register')
     },
     registerPost: (req, res) => {
-        res.send('Successfully registered')
+        let errors = [];
+
+        if(!req.body.firstName) {
+            errors.push({message: 'First name is mondatory'});
+        }
+        if(!req.body.lastName) {
+            errors.push({message: 'Last name is mondatory'});
+        }
+        if(!req.body.email) {
+            errors.push({message: 'Email field is mondatory'});
+        }
+        if(req.body.password !== req.body.passwordConfirm) {
+            errors.push({message: 'Passwords do not match'});
+        }
+
+        if(errors.length > 0) {
+            res.render('default/register', {
+                errors: errors,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email
+
+            });
+        } else {
+            User.findOne({email: req.body.email}).then(user => {
+                if(user) {
+                    req.flash('error-message', 'Email already exists, try to login');
+                    res.redirect('/login');
+                }
+                else {
+                    const newUser = new User(req.body);
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            newUser.password = hash;
+                            newUser.save().then(user => {
+                                req.flash('success-message', 'You are now registered');
+                                res.redirect('/login');
+                            })
+                        })
+                    })
+
+                }
+            })
+        }
+
     }
 }
