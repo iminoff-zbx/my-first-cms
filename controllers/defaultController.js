4cc656541b1a918f8a7d12bbb1e2fdee0abc6aac
navruz-zbx <iminov.zbx@gmail.com>
2020-07-04 10:41:36 +0500

    comment creat done

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,6 +1,7 @@
 const Post = require('../models/PostModel').Post;
 const Category = require('../models/CategoryModel').Category;
 const User = require('../models/UserModel').User;
+const Comment = require('../models/CommentModel').Comment;
 const bcrypt = require('bcryptjs');
 
 
@@ -79,14 +80,42 @@ module.exports = {
 
     },
 
-    singlePost: (req, res) => {
+    getSinglePost: (req, res) => {
         const id = req.params.id;
-        Post.findById(id).lean().then(post => {
+        Post.findById(id)
+            .lean()
+            .populate({path: 'comments', populate: {path: 'user', model: 'user'}})
+            .then(post => {
             if(!post) {
                 res.status(404).json({message: 'No Post found'});
             } else {
-                res.render('default/singlePost', {post: post});
+                res.render('default/singlePost', {post: post, comments: post.comments});
             }
         })
+    },
+
+    submitComment: (req, res) => {
+
+        if(req.user) {
+            Post.findById(req.body.id).lean().then(post => {
+                const newComment = new Comment({
+                    user: req.user.id,
+                    body: req.body.commentBody
+                });
+
+                post.comments.push(newComment);
+                post.save().then(savedPost => {
+                    newComment.save().then(savedComment => {
+                        req.flash('success-message', 'Your comment was submitted for review.');
+                        res.redirect(`/post/${post._id}`);
+                    })
+                })
+            })
+        } else {
+            req.flash('error-message', 'Login first to comment');
+            res.redirect(`/login`);
+        }
+
+
     }
 }
